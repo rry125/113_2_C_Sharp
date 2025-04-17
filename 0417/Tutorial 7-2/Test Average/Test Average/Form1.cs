3e50e785e486103422b8873621b02567248ff9a6
rry125 <A113221029@mail.shu.edu.tw>
2025-04-17 11:58:17 +0800

    0417

---
+++ 0417/Tutorial 7-2/Test Average/Test Average/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Test_Average
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // The Average method accepts an int array argument
+        // and returns the Average of the values in the array.
+        private double Average(int[] scores)
+        {
+            int total = 0;
+
+            foreach (int score in scores)
+            {
+                total += score;
+            }
+
+            return (double)total / scores.Length;
+        }
+
+        // The Highest method accepts an int array argument
+        // and returns the highest value in that array.
+        private int Highest(int[] scores)
+        {
+            int highest = scores[0];
+
+            for (int i = 1; i < scores.Length; i++)
+            {
+                if (scores[i] > highest)
+                {
+                    highest = scores[i];
+                }
+            }
+            return highest;
+        }
+
+        // The Lowest method accepts an int array argument
+        // and returns the lowest value in that array.
+        private int Lowest(int[] scores)
+        {
+            int lowest = scores[0];
+            foreach (int score in scores)
+            {
+                if (score < lowest)
+                {
+                    lowest = score;
+                }
+            }
+            return lowest;
+        }
+
+        private void getScoresButton_Click(object sender, EventArgs e)
+        {
+            const int SIZE = 48;
+            int[] testScores = new int[SIZE];
+            int index = 0;
+            int highestScore = 0;
+            int lowestScore = 0;
+            double averageScore = 0.0;
+            StreamReader inputFile;
+
+            try
+            {
+                if (openFile.ShowDialog() == DialogResult.OK)
+                {
+                    inputFile = File.OpenText(open.FileName);
+                    testScoresListBox.Items.Add(testScores[index]);
+                    index++;
+
+                    while (!inputFile.EndOfStream && index < SIZE)
+                    {
+                        textScores[index] = int.Parse(inputFile.ReadLine());
+                        index++;
+                    }
+                    inputFile.Close();
+
+                    averageScore = Average(testScores);
+                    highestScore = Highest(testScores);
+                    lowestScore = Lowest(testScores);
+
+                    averageScoreLabel.Text = averageScore.ToString("n1");
+                }            
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "錯誤");
+            }
+        }
+
+        
+  
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
