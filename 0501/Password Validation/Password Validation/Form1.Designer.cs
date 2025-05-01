63dc1c386ea04f1c8dc2bb6562ff3701f1cdae37
rry125 <A113221029@mail.shu.edu.tw>
2025-05-01 13:00:14 +0800

    0501

---
+++ 0501/Password Validation/Password Validation/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Password_Validation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.instructionsLabel = new System.Windows.Forms.Label();
+            this.promptLabel = new System.Windows.Forms.Label();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.checkPasswordButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // instructionsLabel
+            // 
+            this.instructionsLabel.Location = new System.Drawing.Point(21, 8);
+            this.instructionsLabel.Name = "instructionsLabel";
+            this.instructionsLabel.Size = new System.Drawing.Size(243, 86);
+            this.instructionsLabel.TabIndex = 0;
+            this.instructionsLabel.Text = resources.GetString("instructionsLabel.Text");
+            this.instructionsLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.instructionsLabel.Click += new System.EventHandler(this.instructionsLabel_Click);
+            // 
+            // promptLabel
+            // 
+            this.promptLabel.AutoSize = true;
+            this.promptLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.promptLabel.Location = new System.Drawing.Point(76, 100);
+            this.promptLabel.Name = "promptLabel";
+            this.promptLabel.Size = new System.Drawing.Size(131, 16);
+            this.promptLabel.TabIndex = 1;
+            this.promptLabel.Text = "Enter a password.";
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(92, 125);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(100, 22);
+            this.passwordTextBox.TabIndex = 2;
+            this.passwordTextBox.TextChanged += new System.EventHandler(this.passwordTextBox_TextChanged);
+            // 
+            // checkPasswordButton
+            // 
+            this.checkPasswordButton.Location = new System.Drawing.Point(64, 156);
+            this.checkPasswordButton.Name = "checkPasswordButton";
+            this.checkPasswordButton.Size = new System.Drawing.Size(75, 33);
+            this.checkPasswordButton.TabIndex = 3;
+            this.checkPasswordButton.Text = "Check Password";
+            this.checkPasswordButton.UseVisualStyleBackColor = true;
+            this.checkPasswordButton.Click += new System.EventHandler(this.checkPasswordButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(145, 156);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 33);
+            this.exitButton.TabIndex = 4;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 201);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.checkPasswordButton);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.promptLabel);
+            this.Controls.Add(this.instructionsLabel);
+            this.Name = "Form1";
+            this.Text = "Password Validation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label instructionsLabel;
+        private System.Windows.Forms.Label promptLabel;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Button checkPasswordButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ 0501/Password Validation/Password Validation/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Password_Validation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void instructionsLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void passwordTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private int NumberLowerCase(string str)
+        {
+            int lowerCaseCount = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsLower(str[i]))
+                {
+                    lowerCaseCount++;
+                }
+            }
+            return lowerCaseCount;
+        }
+
+        private int NumberDigits(string str)
+        {
+            int digits = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsDigit(str[i]))
+                {
+                    digits++;
+                }
+            }
+            return digits;
+        }
+
+        private int NumberUpperCase(string str)
+        {
+            int count = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsDigit(str[i]))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+
+        private void checkPasswordButton_Click(object sender, EventArgs e)
+        {
+            const int MIN_LENGTH = 8;
+            string password = passwordTextBox.Text;
+
+            if (password.Length >= MIN_LENGTH &&
+                NumberUpperCase(password) > 0 &&
+                NumberLowerCase(password) > 0 &&
+                NumberDigits(password) > 0)
+            {
+                MessageBox.Show("密碼有效！", "密碼檢查結果", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("密碼無效！", "密碼檢查結果", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        }
+    
+}
